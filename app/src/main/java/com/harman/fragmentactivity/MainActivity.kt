da1ae64df1d0a619d930a436b11ceb5fadd3c05b
Harmanjot Kaur <109522011+Harmankjot18@users.noreply.github.com>
2022-08-02 17:52:34 +0530

    Fragment Activity

---
+++ app/src/main/java/com/harman/fragmentactivity/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.harman.fragmentactivity
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.FragmentContainer
+import androidx.fragment.app.FragmentContainerView
+import com.harman.fragmentactivity.R.color.green
+import com.harman.fragmentactivity.R.id.fragmentContainerView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnCallFragment : Button
+    lateinit var activityInterface: ActivityInterface
+    lateinit var btnalert_Dialog: Button
+    lateinit var fragmentContainerView : FragmentContainerView
+    var c = 0
+    @SuppressLint("ResourceAsColor")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnCallFragment=findViewById(R.id.btnCallFragment)
+        btnalert_Dialog =findViewById(R.id.btnalert_Dialog )
+        fragmentContainerView =findViewById(R.id.fragmentContainerView)
+
+        btnalert_Dialog.setOnClickListener{
+            c++
+            activityInterface.activityInterface()
+            var alertDialog= AlertDialog.Builder(this)
+            alertDialog.setTitle("Title")
+            alertDialog.setMessage("Alert dialog message")
+            alertDialog.setPositiveButton("Red"){_,_->
+                fragmentContainerView.setBackgroundResource(R.color.red)
+            }
+            alertDialog.setNegativeButton("Green"){_,_->
+                fragmentContainerView.setBackgroundColor(R.color.green)
+            }
+            alertDialog.setNeutralButton("Blue"){_,_->
+                fragmentContainerView.setBackgroundColor(R.color.blue)
+            }
+            alertDialog.show()
+        }
+    }
+}
